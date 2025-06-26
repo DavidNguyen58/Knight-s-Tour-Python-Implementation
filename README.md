84f90d8f1d946f96a0a7e103358161dde5f93f1e
DavidNguyen58 <138356579+DavidNguyen58@users.noreply.github.com>
2025-06-26 12:58:25 +0700

    Initialised the repo

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Knight-s-Tour-Python-Implementation
+# Knight-Tour-Python-Implementation
+
+## Introduction
+The knight's tour is one of popular problems in Computer Science where the problem seeks to find if there is a tour of a knight which visits all the squares on the board of size n*n, and every square can not be re-visited.
+
+## Approach
+The problem could be solved via backtracking where we try to consider all the possible moves at the current position of the knight the knight could take and extend the search. If the knight hits a dead end, where no further search is possible, then we backtrack to the previous position and search other moves. If there is no solution after all the possible searches, then we return False. Otherwise, we will return True and print out the tour of the Knight. The time complexity if this brute-force algorithm would be $O(8^n)$ where 8 is the number of possible moves at every position and n is the number of squares on the board.
+
+However, we could be a little more clever here by including a heuristic in our search. One of the popular heuristics for this problem is Warnsdorf Heuristic. During the exploration state, the heuristic will move the Knight to a position with the least number of possible moves, helping to increase the speed of finding the solution if it exits. Additionally, the heuristic also allows to generate a possible solution for a large state of n greater than 20.
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,86 @@
+import time
+def print_board(state):
+    for row in state:
+        print(row)
+
+
+def board(n):
+    return [[0] * n for _ in range(n)]
+
+
+def validate(pos, n):
+    x, y = pos
+    return 0 <= x < n and 0 <= y < n
+
+
+def warnsdorf_heuristic(pos, dirs, visited, n):
+    """
+    Heuristic Search: Move the knight to a position with the least possible number of moves
+    """
+    def calculate_moves(pos, dirs, visited, n):
+        x, y = pos
+        count = 0
+
+        for dx, dy in dirs:
+            nx, ny = x + dx, y + dy
+            if not validate((nx, ny), n) or (nx, ny) in visited:
+                continue
+            count += 1
+        
+        return count
+    
+    x, y = pos
+    moves = []
+
+    for dx, dy in dirs:
+        nx, ny = x + dx, y + dy
+        if not validate((nx, ny), n) or (nx, ny) in visited:
+            continue
+        count = calculate_moves((nx, ny), dirs, visited, n)
+        moves.append((count, (nx, ny)))
+    
+    moves.sort(key=lambda x: x[0])
+    return [move for _, move in moves]
+
+
+def search(state, src, dirs, dst, visited):
+    """
+    DFS Search Method
+    """
+    N = len(state)
+    x, y = src
+    visited.add(src)
+    state[x][y] = dst
+
+    if len(visited) == len(state) * len(state[0]):
+        return True
+    
+    for nx, ny in warnsdorf_heuristic(src, dirs, visited, N):
+        if not validate((nx, ny), len(state)) or (nx, ny) in visited:
+            continue
+        if search(state, (nx, ny), dirs, dst + 1, visited):
+            return True
+    
+    visited.remove(src)
+    state[x][y] = 0
+    return False
+
+if __name__ == "__main__":
+    n = 5
+
+    state = board(n)
+    dirs = [
+        (2, 1), (1, 2), (-1, 2), (-2, 1),
+        (-2, -1), (-1, -2), (1, -2), (2, -1)
+    ]
+
+    src = (0, 0)
+    visited = set()
+
+    start = time.time()
+    ans = search(state, src, dirs, 0, visited)
+    end = time.time()
+
+    print(f"The time it takes to run {end - start}.")
+    if ans:
+        print_board(state)
